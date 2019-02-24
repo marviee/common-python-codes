559911a6dd460491f0fd19a611138aa0e113f369
marviee <37642775+marviee@users.noreply.github.com>
2019-02-25 00:57:06 +0100

    Update mode.py

--- mode.py
+++ mode.py
@@ -7,10 +7,12 @@ m= max(lst)
 ml = [x for x in data if data.count(x)==m ] #to find most frequent values
 print(ml)
 mode = []
+for x in ml: #to remove duplicates of mode
+        if x not in mode:
+            mode.append(x)
 
 
-
-def mode(data):
+def mode(data):               #using function to solve for mode
     lst =[]
     hgh=0
     for i in range(len(data)):
@@ -24,14 +26,3 @@ def mode(data):
             mode.append(x)
     return mode
 #print (mode([1,2,2,2,7,7,7,5,5,5]))
-
-
-
-'''data=[1,3,4,5,5,5,5,3,3,3,9,8,0,4,33]
-lst =[]
-hgh=0
-for i in range(len(data)):
-    lst.append(data.count(data[i]))
-m= max(lst)
-print(m)
-'''
